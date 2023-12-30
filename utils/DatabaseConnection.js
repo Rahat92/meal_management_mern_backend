d7d9508270395809fe93d3d2d6835bf1eb99caf6
khrahat92 <khrahat92@gmail.com>
2023-12-30 23:03:52 +0600

    database atlas

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -2,9 +2,12 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/messManagement", {
-      useNewurlParser: true,
-    })
+    .connect(
+      "mongodb+srv://khrahat92:1ZP6L8yY7fagaXwj@cluster0.c8qwi24.mongodb.net/mealManagement",
+      {
+        useNewurlParser: true,
+      }
+    )
     .then(() => console.log("Successfully connect Database with the app"));
 };
 
