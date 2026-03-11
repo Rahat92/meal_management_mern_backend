eb5433ab4e50fee4d6d4c29e811bec4793da704b
MidwaySecurities <it@midwaybd.com>
2026-03-11 13:33:51 +0600

    add header in response

--- app.js
+++ app.js
@@ -33,7 +33,18 @@ app.use("/public", express.static("public"));
 app.use(express.json());
 
 app.use(cookieParser());
+app.use((req,res,next)=>{
+  res.header("Access-Control-Allow-Origin","https://kikhaben.vercel.app");
+  res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,PATCH,OPTIONS");
+  res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials","true");
 
+  if(req.method === "OPTIONS"){
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 // Routes
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/meal", mealCountRouter);
