ba83a6c79d597717b0a5c7d7614147af4b03edfa
MidwaySecurities <it@midwaybd.com>
2026-03-12 12:11:16 +0600

    mongo-uri in env

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -1,25 +1,55 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const connectToDb = () => {
-  mongoose
-    .connect(
-      "mongodb+srv://khrahat92:1ZP6L8yY7fagaXwj@cluster0.c8qwi24.mongodb.net/mealManagement",
-      // "mongodb://localhost:27017/mealManagement",
-      {
-        useNewurlParser: true,
-      }
-    )
-    .then(() => console.log("Successfully connect Database with the app"));
-};
 // const connectToDb = () => {
 //   mongoose
 //     .connect(
-//       "mongodb://127.0.0.1:27017/mealManagement",
+//       "mongodb+srv://khrahat92:1ZP6L8yY7fagaXwj@cluster0.c8qwi24.mongodb.net/mealManagement",
+//       // "mongodb://localhost:27017/mealManagement",
 //       {
 //         useNewurlParser: true,
 //       }
 //     )
 //     .then(() => console.log("Successfully connect Database with the app"));
 // };
+// // const connectToDb = () => {
+// //   mongoose
+// //     .connect(
+// //       "mongodb://127.0.0.1:27017/mealManagement",
+// //       {
+// //         useNewurlParser: true,
+// //       }
+// //     )
+// //     .then(() => console.log("Successfully connect Database with the app"));
+// // };
+
+// module.exports = connectToDb;
+
+
+
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectToDb = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  cached.conn = await cached.promise;
+
+  console.log("MongoDB connected");
+  return cached.conn;
+};
 
 module.exports = connectToDb;
