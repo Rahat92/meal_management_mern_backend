e0f3363f6b353b2a938d3ff3e9634c39ad3bbeec
Rahat92 <khrahat92@gmail.com>
2026-04-16 08:38:08 +0600

    update from home

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -15,15 +15,17 @@ exports.get_all_users = catchAsyncError(async (req, res, next) => {
     const managerId = req.query.managerId;
     console.log('haha', managerId)
     const page = req.query.page || 1;
-    const limit = req.query.limit || 3;
+    const limit = req.query.limit || 2;
     const offset = parseInt(page - 1) * parseInt(limit);
     const users = await User.find({ manager: managerId }).select("-password -passwordConfirm -__v -createdAt -updatedAt").skip(offset).limit(parseInt(limit));
     const totalUsers = await User.countDocuments({ manager: managerId });
+    const totalPages = Math.ceil(totalUsers / limit);
     res.status(200).json({
         status: "success",
         data: {
             users,
-            totalUsers
+            totalUsers,
+            totalPages
         }
     });
 })
