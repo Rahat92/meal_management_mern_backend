2951a0e26f6eccf7f6f8f853f1ff18abe709d402
MidwaySecurities <it@midwaybd.com>
2026-04-16 16:15:51 +0600

    update search pagination

--- controllers/v1/userController.js
+++ controllers/v1/userController.js
@@ -13,12 +13,12 @@ exports.get_all_managers = catchAsyncError(async (req, res, next) => {
 
 exports.get_all_users = catchAsyncError(async (req, res, next) => {
     const managerId = req.query.managerId;
-    console.log('haha', managerId)
     const page = req.query.page || 1;
     const limit = req.query.limit || 2;
+    const search = req.query.search || "";
     const offset = parseInt(page - 1) * parseInt(limit);
-    const users = await User.find({ manager: managerId }).select("-password -passwordConfirm -__v -createdAt -updatedAt").skip(offset).limit(parseInt(limit));
-    const totalUsers = await User.countDocuments({ manager: managerId });
+    const users = await User.find({ manager: managerId, name: { $regex: search, $options: "i" } }).select("-password -passwordConfirm -__v -createdAt -updatedAt").skip(offset).limit(parseInt(limit));
+    const totalUsers = await User.countDocuments({ manager: managerId, name: { $regex: search, $options: "i" } });
     const totalPages = Math.ceil(totalUsers / limit);
     res.status(200).json({
         status: "success",
